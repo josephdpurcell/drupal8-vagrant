9802611fdaa743a4a11f64aa1b6c8e7f2114a93e
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-02-25 13:27:18 -0600

    Tweak how install is run

--- provisioning/bin/install.sh
+++ provisioning/bin/install.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 SILENT=0
-CWD=`pwd`
-
-if [ "$CWD" != "/var/www/dev1.d8.local" ]
-then
-    echo "You must run this inside /var/www/dev1.d8.local"
-    exit 1
-fi
 
 if [ "$1" == "--quiet" ]
 then
     echo "Entering quiet mode. No user interaction will take place."
 fi
+
+cd /var/www/dev1.d8.local
 
 provisioning/bin/install_drupal.sh
 if [ ! $SILENT ]
