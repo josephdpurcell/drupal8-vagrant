37e4ccec8ec8da483e9f5f5b0a5301f522d615cf
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-02-25 12:04:38 -0600

    Update test script

--- provisioning/bin/test.sh
+++ provisioning/bin/test.sh
@@ -3,5 +3,5 @@
 #php core/scripts/run-tests.sh --module workbench_moderation
 
 echo "Examples":
-echo 'run-tests.sh --xml /var/www/dev1.d8.local/web/core/test_results --class "Drupal\multiversion\Tests\WorkspaceTest"'
-echo 'run-tests.sh --xml /var/www/dev1.d8.local/web/core/test_results --module workbench_moderation'
+echo 'run-tests.sh --verbose --class "Drupal\multiversion\Tests\WorkspaceTest"'
+echo 'run-tests.sh --verbose --module workbench_moderation'
