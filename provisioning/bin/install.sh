cf4eba47241bf018fec497b38e0ba6a484028b2a
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-06-29 11:59:13 -0500

    Comment out problematic drupal install steps

--- provisioning/bin/install.sh
+++ provisioning/bin/install.sh
@@ -7,13 +7,6 @@ then
     echo "Entering quiet mode. No user interaction will take place."
 fi
 
-cd /var/www/dev1.d8.local
+#cd /var/www/dev1.d8.local
 
-provisioning/bin/drupal_install.sh
-if [ ! $SILENT ]
-then
-    provisioning/bin/modules_install.sh
-else
-    echo "Skipping installation of modules, because quiet mode."
-fi
-provisioning/bin/config_update.sh
+#provisioning/bin/drupal_install.sh
