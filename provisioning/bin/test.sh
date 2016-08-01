4d577b15f76f18742bcbef5da739fba6ed7b5c66
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-08-01 16:04:24 -0500

    Show example with phpunit

--- provisioning/bin/test.sh
+++ provisioning/bin/test.sh
@@ -4,3 +4,9 @@ echo "Examples":
 echo 'export SIMPLETEST_DB=mysql://root:pass@localhost/default; ../vendor/bin/phpunit --group now'
 echo 'run-tests.sh --verbose --class "Drupal\multiversion\Tests\WorkspaceTest"'
 echo 'run-tests.sh --verbose --module workbench_moderation'
+
+echo 'php ./core/scripts/run-tests.sh --color --verbose --dburl mysql://root:pass@localhost/d8 --class "Drupal\Tests\workspace\Functional\ReplicatorManagerTest"'
+
+# This is how to do it with phpunit:
+echo 'vagrant@sites:/var/www/sites.local/d8/docroot/core$ sudo -u www-data SIMPLETEST_DB=mysql://root:pass@localhost/d8 SIMPLETEST_BASE_URL=http://sites.local ../../vendor/bin/phpunit -v /var/www/sites.local/d8/docroot/modules/contrib/workspace/tests/src/Functional/ReplicationSettingsTest.php'
+
